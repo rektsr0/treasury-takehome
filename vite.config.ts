6c3ddae32b06ca0a41a2e0950c04fc3ed62543bf
rektsr0 <srpowerwashing1@gmail.com>
2026-07-17 20:00:07 -0400

    Prepare GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/treasury-takehome/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+}));
